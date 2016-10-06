ef3e50c26943b9673d98a260a2c6e45f1b8977ab
Pranav Lakshminarayanan <Pranav Lakshminarayanan>
2016-10-06 13:11:41 -0400

    run on public ip

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-/bin/registrator -ip="19.12.12.12" consul://localhost:8500
+priv_addr=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+pub_addr=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+
+/bin/registrator -ip=$pub_addr consul://localhost:8500
