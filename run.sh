bcda944007c1b63a3011056171b9177442ce2900
Pranav Lakshminarayanan <Pranav Lakshminarayanan>
2016-10-06 14:33:54 -0400

    is the error in run.sh?

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 priv_addr=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 pub_addr=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
-/bin/registrator -ip=$pub_addr consul://localhost:8500
+echo $priv_addr
+echo $pub_addr
+
+# /bin/registrator -ip=$pub_addr consul://localhost:8500
